e52b9be5b01b3b26346c955d2ab2b064ec3fc85a
ledgerluminary55 <ledgerluminary55@gmail.com>
2023-02-12 21:41:33 +0100

    add ability to decide winner

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::Write;
 use std::io::stdin;
 use std::io::stdout;
-#[derive(Debug, Clone, Copy)]
+use std::io::Write;
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Player {
     X,
     O,
@@ -57,6 +57,10 @@ fn main() {
         let y = x / 3;
         let z = x % 3;
 
+        if x > 8 || board.grid[y][z].is_ok() {
+            continue;
+        }
+
         board.grid[y][z] = Ok(board.current_turn);
 
         //if board.grid[y][z].is_ok() || x > 8 {
@@ -78,9 +82,51 @@ fn main() {
             println!("-------------");
         }
 
+        for row in board.grid {
+            if row[0] == row[1] && row[1] == row[2] && row[0].is_ok() {
+                board.winner = row[0]
+            }
+        }
+
+        for i in 0..3 {
+            if board.grid[0][i] == board.grid[1][i]
+                && board.grid[1][i] == board.grid[2][i]
+                && board.grid[0][i].is_ok()
+            {
+                board.winner = board.grid[0][i];
+            }
+        }
+
+        if board.grid[0][0] == board.grid[1][1]
+            && board.grid[1][1] == board.grid[2][2]
+            && board.grid[0][0].is_ok()
+        {
+            board.winner = board.grid[0][0];
+        }
+        if board.grid[0][2] == board.grid[1][1]
+            && board.grid[1][1] == board.grid[2][0]
+            && board.grid[0][2].is_ok()
+        {
+            board.winner == board.grid[0][2];
+        }
+
+
+        match board.winner {
+            Ok(Player::X) => {
+                println!("X wins!");
+                break;
+            }
+            Ok(Player::O) => {
+                println!("O wins!");
+                break;
+            }
+            Err("") => (),
+            _ => println!("Draw!"),
+        }
+
         board.current_turn = match board.current_turn {
             Player::X => Player::O,
-            Player::O => Player::X
+            Player::O => Player::X,
         }
     }
 }
